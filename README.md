1d71beb9026824b2def8f9cab46f660280959abe
lizijie <leechikit0823@163.com>
2016-06-20 16:39:58 +0800

    modify

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ybruin-site
+
+进入src命令行输入:
+ybruin release dev 产出开发目录
+ybruin release build 产出生产目录
--- src/ybruin-conf.js
+++ src/ybruin-conf.js
@@ -1,6 +1,19 @@
 fis.config.set('projectConf',{
     name:'ybruin-site',
     terminal:'pc',
-    domainList:['http://res.cont.yy.com']
+    domainList:['http://res.cont.yy.com'],
+    outputPath:'../dist',
+    compressCss:true,
+    compressImg:true,
+    compressJs:true,
+    useSprite:true,
+    useHash:true
+})
+
+fis.config.set('projectConf.useSprite',{
+	to:'/images/sprites',
+    margin:2
+})
+
 })
 fis.runConf();
