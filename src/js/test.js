2e98496b962af93b80ead4fb100e76b2a8df5e7a
lizijie <leechikit0823@163.com>
2016-07-27 20:18:54 +0800

    update

---
+++ src/js/test.js
@@ -0,0 +1,6 @@
+var ModPic = require('../components/mod-pic');
+
+ReactDOM.render(
+	<ModPic id="44" size="10" />,
+	document.getElementById('example')
+)
--- src/ybruin-conf.js
+++ src/ybruin-conf.js
@@ -1,19 +1,21 @@
-fis.config.set('projectConf',{
-    name:'ybruin-site',
-    terminal:'pc',
-    domainList:['http://res.cont.yy.com'],
-    compDomain:'http://res.cont1.yy.com',
-    outputPath:'../dist',
-    compressCss:true,
-    compressImg:true,
-    compressJs:true,
-    useSprite:true,
-    useHash:true
+fis.config.set('projectConf', {
+    name: 'ybruin-site', //项目名称
+    terminal: 'pc', //设备端分类:pc,mobild
+    domainList: ['http://res.cont.yy.com'], //静态资源CDN
+    compDomain: 'http://res.cont.yy.com', //组件资源CDN
+    outputPath: '../dist', //项目产出路径
+    compressCss: true, //开启css文件压缩
+    compressImg: true, //开启图片文件压缩
+    compressJs: true, //开启js文件压缩
+    useSprite: true, //开启雪碧图
+    useHash: true, //开启MD5文件戳
+    useModule: 'cmd' //模块化模式:amd,cmd,commonjs,false
+
 })
 
-fis.config.set('projectConf.useSprite',{
-	to:'/images/sprites',
-    margin:2
+fis.config.set('projectConf.useSprite', {
+    to: '/images/sprites', //雪碧图产出路径
+    margin: 2 //图片间隔
 })
 
 fis.runConf();
